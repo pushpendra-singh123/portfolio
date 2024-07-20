1435585d97fcf3c3ae6cbc88234ac1acd8c863c1
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-20 22:51:30 +0530

    give margin to hero from left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl mx-20">
               Pushpendra Singh
             </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-xl tracking-tight text-transparent mx-20">
               Front-end Developer
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter mx-20">
               {/*change for left space is mx-20 also in upper span tag */}
               {HERO_CONTENT}
             </p>
