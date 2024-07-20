2c7185c13804082aa3e53a58a30ba1647f98193a
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-21 02:15:52 +0530

    better response

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,20 +27,23 @@ const Projects = () => {
                 alt={project.title}
                 width={150}
                 height={150}
-                className="mb-6 rounded"
+                className="mx-2 mb-6 rounded"
               />
             </motion.div>
-            <motion.div 
-            whileInView={{opacity:1,x:0}}
-            initial={{opacity:0,x:100}}
-            transition={{duration:1}}
-            className="w-full max-w-xl lg:w-3/4">
-              <h6 className="mb-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className=" mx-2 w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className=" mx-4 mb-2 font-semibold">{project.title}</h6>
+              <p className=" mx-4 mb-4 text-neutral-400">
+                {project.description}
+              </p>
               {project.technologies.map((tech, index) => (
                 <span
                   key={index}
-                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                  className="mx-4 mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
                 >
                   {tech}
                 </span>
