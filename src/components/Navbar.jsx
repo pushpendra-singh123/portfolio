d9bb0dde8410e1b729df155bd45ec294b89a9531
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-20 13:19:49 +0530

    adding icons in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
+import logo from "../assets/kevinRushLogo.png"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaTwitterSquare />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
