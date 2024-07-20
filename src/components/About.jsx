e852a369d83d9e6df1e90ed846d9dce568ec41d7
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-20 17:23:30 +0530

    adding about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl">About Me</h1>
+      <h1 className="my-20 text-center text-4xl">
+        About Me
+        <span></span>
+      </h1>
     </div>
   );
 };
 
 export default About;
-fc;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,19 +6,19 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl mx-16">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Pushpendra Singh
             </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent mx-16">
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Front-end Developer
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter mx-20">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {/*change for left space is mx-20 also in upper span tag */}
               {HERO_CONTENT}
             </p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-0">
+        <div className="w-full lg:w-1/2 lg:p-6">
           {/*Change Padding correctly */}
           <div className="flex justify-center">
             <img src={profilePic} alt="Pushpendra Singh" />
