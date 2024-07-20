5af5e92c771471f246cb57eb66a5a7f704a62898
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-21 02:02:33 +0530

    make it more responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl md:text-5xl lg:text-6xl">
+      <h1 className="my-20 text-center text-4xl">
         About
         <span className="text-neutral-500 "> Me</span>
       </h1>
-      <div className="flex flex-wrap lg:flex-nowrap">
+      <div className="flex flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
@@ -28,10 +28,10 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 xl:w-2/3 p-4 lg:p-8"
+          className="w-full lg:w-1/2"
         >
           <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+            <p className="mx-4 my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
         </motion.div>
       </div>
